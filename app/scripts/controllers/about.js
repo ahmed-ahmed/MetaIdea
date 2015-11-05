9b1ca9212f9acefe9b31df7002e009d7b54035c1
aahmed@mum.edu <rseco82>
2015-11-04 19:40:53 -0500

    Adding the Yo generated code

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name metaIdeaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the metaIdeaApp
+ */
+angular.module('metaIdeaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name metaIdeaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the metaIdeaApp
+ */
+angular.module('metaIdeaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
